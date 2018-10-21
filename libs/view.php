ac0e3fbc8afdd16e7e257d9b6bb8ef0e5b416624
Артем <19arty87@gmail.com>
2018-10-21 15:14:43 +0300

    Создан класс для работы с представлениями

--- libs/view.php
+++ libs/view.php
@@ -3,5 +3,12 @@ namespace Libs;
 
 class View
 {
+    public function render($template, $data)
+    {
+        extract($data);
 
+        include "./templates/common/header.php";
+        include "./templates/" . $template . ".php";
+        include "./templates/common/footer.php";
+    }
 }
